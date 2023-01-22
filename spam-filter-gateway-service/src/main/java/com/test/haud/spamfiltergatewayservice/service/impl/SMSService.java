0423f4941c4833a8eff6a9708ce55129e4e51cff
ramazansakin <ramazansakin63@gmail.com>
2023-01-22 22:27:17 +0300

    feat: I tried to add sms batching to reduce round-trip to send sms messages to rabbitMq but it doest effect much maycbe it can effect for little bigger time slots than 1 sec

--- spam-filter-gateway-service/src/main/java/com/test/haud/spamfiltergatewayservice/service/impl/SMSService.java
+++ spam-filter-gateway-service/src/main/java/com/test/haud/spamfiltergatewayservice/service/impl/SMSService.java
@@ -7,17 +7,54 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SMSService {
 
     private final RabbitTemplate rabbitTemplate;
-
     private final BlockedDestinationService blockedDestinationService;
 
     @Value("${spring.rabbitmq.queue}")
     private String queue;
+//
+//    private final Queue<SMS> smsBuffer = new LinkedList<>();
+//    private final int bufferSize = 500; // adjust this number as needed
+//    private final long batchInterval = 450; // adjust this number as needed, in milliseconds
+//
+//    public SMSService(RabbitTemplate rabbitTemplate, BlockedDestinationService blockedDestinationService) {
+//        this.rabbitTemplate = rabbitTemplate;
+//        this.blockedDestinationService = blockedDestinationService;
+//        startBufferThread();
+//    }
+//
+//    private void startBufferThread() {
+//        Thread bufferThread = new Thread(() -> {
+//            while (true) {
+//                try {
+//                    Thread.sleep(batchInterval);
+//                } catch (InterruptedException e) {
+//                    log.error("Error while sleeping buffer thread", e);
+//                }
+//                if (!smsBuffer.isEmpty()) {
+//                    List<SMS> smsBatch = new ArrayList<>();
+//                    int count = 0;
+//                    while (!smsBuffer.isEmpty() && count < bufferSize) {
+//                        smsBatch.add(smsBuffer.poll());
+//                        count++;
+//                    }
+//                    log.debug("Sending batch of {} SMS messages to charging service", smsBatch.size());
+//                    rabbitTemplate.convertAndSend(queue, smsBatch);
+//                }
+//            }
+//        });
+//        bufferThread.start();
+//    }
 
     public void receiveSMS(SMS sms) {
         // submit messages (fire-and-forget) to the charging module irrespective of the spam filter`s outcome
